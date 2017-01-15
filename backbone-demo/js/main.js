698bfd26ca9df3e2928a727d37c7a752ae98522a
王顶 <408542507@qq.com>
2017-01-15 09:14:21 +0800

    增加了我的代码

---
+++ backbone-demo/js/main.js
@@ -0,0 +1,45 @@
+/**
+ * Created by 王顶 on 2017/1/15.
+ */
+
+(function () {
+window.App = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Router: {}
+};
+
+App.Router = Backbone.Router.extend({
+    routes: {
+        '': 'index',
+        'show/:id': 'show',
+        'download/*random': 'download',
+        'search/:query': 'search',
+        '*other': 'default'
+    },
+
+    index: function () {
+        $(document.body).append("调用了 Index 路由<br>");
+    },
+
+    show: function (id) {
+        $(document.body).append("调用了 show 路由，参数等于 " + id + "<br>");
+    },
+
+    download: function (random) {
+        $(document.body).append("调用了 download 路由，参数等于 " + random + <br>");
+    },
+
+    search: function (query) {
+        $(document.body).append("调用了 search 路由，参数等于 " + query + <br>");
+    },
+
+    default: function (other) {
+        $(document.body).append("你访问的 " + other + " 路由未定义<br>");
+    }
+});
+
+new App.Router();
+Backbone.history.start();
+})();
